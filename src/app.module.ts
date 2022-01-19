3078206243076e00f6248dc49d47dde8f2b86866
Mohammad javad Mehdizadeh <mj.mehdizadeh93@gmail.com>
2022-01-19 12:59:15 +0330

    run application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
+import { BootstrapModule } from "./bootstrap/bootstrap.module";
 
 @Module({
-  imports: [],
+  imports: [BootstrapModule],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from "./swagger";
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
   setupSwagger(app)
-  await app.listen(4200);
+  await app.listen(configService.get("PORT"));
+  console.info(`Application is running on ${await app.getUrl()}`);
+
 }
 bootstrap();
