8f245aba916cc537862e47fafaa5c7ffed5a9363
Mohammad javad Mehdizadeh <mj.mehdizadeh93@gmail.com>
2022-01-19 13:26:51 +0330

    export bootstrap module and change to global

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from "@nestjs/common";
 import { BootstrapModule } from "./bootstrap/bootstrap.module";
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
+@Global()
 @Module({
   imports: [BootstrapModule, AuthModule, UsersModule],
+  exports: [BootstrapModule]
 })
 export class AppModule {}
