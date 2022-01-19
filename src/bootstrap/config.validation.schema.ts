fc5338bf52e373f96854370cf3a45f437d5e2411
Mohammad javad Mehdizadeh <mj.mehdizadeh93@gmail.com>
2022-01-19 12:53:17 +0330

    .env example

--- src/bootstrap/config.validation.schema.ts
+++ src/bootstrap/config.validation.schema.ts
@@ -5,14 +5,12 @@ export default Joi.object({
   APP_ENV: Joi.string()
     .valid('development', 'production')
     .default('development'),
-  APP_URL: Joi.string().default('http://localhost:9000'),
   PORT: Joi.number().default(4200),
   MONGO_URL: Joi.string()
     .required()
     .regex(/^mongodb/),
   JWT_SECRET: Joi.string().required(),
-  ACCESS_JWT_TTL: Joi.number().required(),
-  REFRESH_JWT_TTL: Joi.number().required(),
+  JWT_TTL: Joi.number().default(9000),
   MAIL_HOST: Joi.string().required(),
   MAIL_PORT: Joi.number().required(),
   MAIL_USERNAME: Joi.string(),
